a4b67a1c3a59ba0798cbc81e87dc4a2e0cdb048a
sunxifa <sunxf_blue@163.com>
2018-09-03 16:34:28 +0800

    second commit

---
+++ myPrint.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+print 'The quick brown fox', 'jumps over', 'the lazy dog'
+print '100 + 200 =', 100 + 200
+print u'中文'
---
+++ myinput.py
@@ -0,0 +1,2 @@
+name = raw_input("Please input your name :")
+print 'hello,', name
